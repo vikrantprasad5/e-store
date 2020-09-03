8be902e3dcc0fc28203ac8a7748c7eeab25e8de8
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-03 17:57:58 +0530

    Error page added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
 export default function Error() {
-  return <h1>hello form error page</h1>;
+  return <section className="error-page section">
+    <div className="error-container">
+      <h1>oops! it's a dead end</h1>
+      <Link to="/home" className="btn btn-primary">
+        back to home
+      </Link>
+    </div>
+  </section>;
 }
