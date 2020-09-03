48d2a304e3fb6937dba915e71ebedbc209ff3e7f
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-03 18:10:45 +0530

    about us added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+  <section className="section about-section">
+    <h1 className="section-title">About Us</h1>
+    <p>
+      I am a talented, ambitious and hardworking individual, 
+      with broad skills and experience in digital and printed 
+      marketing, social media and leading projects.
+      Furthermore, I am adept at handling multiple tasks on a 
+      daily basis competently and at working well under pressure.
+      A key strength is communication; building strong 
+      relationships with people in order to deliver the best 
+      results.
+    </p>
+  </section>);
 }
