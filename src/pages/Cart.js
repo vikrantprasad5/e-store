0ceb194c28fc0d71c1a4d3dc36ea9242e909736b
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-19 09:49:23 +0530

    Cart Context setup

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import {CartContext} from '../context/cart'
 export default function Cart() {
+  const {cart,total} = React.useContext(CartContext);
+  //console.log({cart,total});
   return <h1>hello from cart page</h1>;
 }
