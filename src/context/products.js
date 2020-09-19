1f5f6b3456885febedda80f35801c89d7280e815
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-19 08:53:10 +0530

    Added featured Products on home page

--- src/context/products.js
+++ src/context/products.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import axios from "axios";
 import url from "../utils/URL";
+import {featuredProducts} from '../utils/helpers';
 export const ProductContext = React.createContext();
 //Provider, Consumer, useContext
 
@@ -13,7 +14,9 @@ export default function ProductProvider({children}) {
     React.useEffect(()=>{
         setLoading(true);
         axios.get(`${url}/products`).then(response=>{
-            setProducts(response.data)
+            const featured = featuredProducts(response.data);
+            setProducts(response.data);
+            setFeatured(featured);
             setLoading(false);
         });
         return ()=>{};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
 import {Link} from "react-router-dom"
+import FeaturedProducts from "../components/Products/FeaturedProducts";
+
 export default function Home() {
-  return <React.Fragment>
+  return <>
     <Hero>
       <Link to="/products" className="btn btn-primary btn-hero">
         Our Products
       </Link>
     </Hero>
-  </React.Fragment>;
+    <FeaturedProducts/>
+  </>;
 }
