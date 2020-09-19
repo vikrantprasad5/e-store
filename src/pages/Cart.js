ef786f744e60b5de4b3e3979f43336bad4f6d78b
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-19 11:45:29 +0530

    Empty Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,15 @@
 import React from "react";
-import {CartContext} from '../context/cart'
+import {CartContext} from '../context/cart';
+import EmptyCart from '../components/Cart/EmptyCart';
+import CartItem from '../components/Cart/CartItem';
+import {Link} from 'react-router-dom';
+// import {UserContext} from '../context/user'
 export default function Cart() {
+  let user = false;
   const {cart,total} = React.useContext(CartContext);
   //console.log({cart,total});
+  if(cart.length === 0){
+    return <EmptyCart/>;
+  }
   return <h1>hello from cart page</h1>;
 }
