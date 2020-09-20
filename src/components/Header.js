5afc33d7c116bf36392b1b774762f7c68dafc431
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-20 11:37:37 +0530

    Implemented cart item notification feature to tell the number of items in cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
+import CartLink from "./Cart/CartLink"
 export default function Header() {
   return <header className="header">
     <img src={logo} alt="vintage tech logo" className="logo"></img>
@@ -20,10 +21,8 @@ export default function Header() {
         <div>
           <li>
             <Link to="/login">Login</Link>
-          </li> 
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>  
+          </li>
+          <CartLink/>  
         </div>
 
       </ul>
